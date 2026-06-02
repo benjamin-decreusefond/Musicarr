70dce5098ef4c85bd12b7d353026df6c30d49b18
BenjaminDecreusefond <benjamin.decreusefond@weezevent.com>
2026-06-02 13:00:38 +0200

    Configure JSON options for controllers
    Added JSON options to controllers for enum serialization.

--- src/Musicarr.Api/Program.cs
+++ src/Musicarr.Api/Program.cs
@@ -51,7 +51,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         };
     });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
